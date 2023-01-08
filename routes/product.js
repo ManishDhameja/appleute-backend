22d086db3cabe10781d50dd40d7f03ec840864b1
ManishDhameja <manishdhameja86@gmail.com>
2023-01-08 15:20:34 +0530

    Added Total Cart Api

--- routes/product.js
+++ routes/product.js
@@ -7,5 +7,6 @@ router.get("/getAll", productController.getAllItems);
 router.get("/findById", productController.findById);
 router.get("/addToCart", productController.addToCart);
 router.get("/removeFromCart", productController.removeFromCart);
+router.get("/getCartTotal", productController.getCartTotal);
 
 export default router;
